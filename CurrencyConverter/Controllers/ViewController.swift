769c5ca61e8ab59782bd8e1a949b651febed7646
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-18 00:18:12 +0300

    added variables

--- CurrencyConverter/Controllers/ViewController.swift
+++ CurrencyConverter/Controllers/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var firstEllipse: UIView!
 
+    var isSell = true
+    var isBuy = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
