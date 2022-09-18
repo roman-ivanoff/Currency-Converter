ec80d911c8ec1668048444d5757552c31c49f219
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-18 17:18:30 +0300

    added converterView

--- CurrencyConverter/Controllers/ViewController.swift
+++ CurrencyConverter/Controllers/ViewController.swift
@@ -9,14 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var firstEllipse: UIView!
-
+    @IBOutlet weak var converterView: UIView!
+    
     var isSell = true
     var isBuy = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setConverterView()
     }
 
+    private func setConverterView() {
+        converterView.layer.cornerRadius = 10
 
+        converterView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+        converterView.layer.shadowOpacity = 1
+        converterView.layer.shadowRadius = 4
+        converterView.layer.shadowOffset = CGSize(width: 0, height: 4)
+    }
 }
 
