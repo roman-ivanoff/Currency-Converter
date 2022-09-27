0a0218e6f5084a123e431f7abea29b0ce93e3919
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-27 20:37:32 +0300

    added functional

--- CurrencyConverter/Extensions/Date+Comparison.swift
+++ CurrencyConverter/Extensions/Date+Comparison.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Date {
+    var hasHourPassed: Bool {
+        let currentDate = Calendar.current.dateComponents([.hour, .day, .month, .year], from: Date())
+        let lastUpdateDate = Calendar.current.dateComponents([.hour, .day, .month, .year], from: self)
+
+        return currentDate.hour ?? 0 > lastUpdateDate.hour ?? 0 ||
+            currentDate.day ?? 0 > lastUpdateDate.day ?? 0 ||
+            currentDate.month ?? 0 > lastUpdateDate.month ?? 0 ||
+            currentDate.year ?? 0 > lastUpdateDate.year ?? 0
+    }
+}
