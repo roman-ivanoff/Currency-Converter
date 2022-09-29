ba5e15b2ad609de9dd692d08bfe1708e0a5ebaeb
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-29 10:55:03 +0300

    added extension CurrencyRate

---
+++ CurrencyConverter/Extensions/CurrencyRate+Initialization.swift
@@ -0,0 +1,19 @@
+//
+//  CurrencyRate+Initialization.swift
+//  CurrencyConverter
+//
+//  Created by Roman Ivanov on 29.09.2022.
+//
+
+import Foundation
+
+extension CurrencyRate {
+    init(response source: ExchangeRate) {
+        self.init(
+            base: Currency(rawValue: source.baseCurrency.rawValue),
+            currency: Currency(rawValue: source.currency ?? "Not Found"),
+            sale: Decimal(source.saleRateNB),
+            purchase: Decimal(source.purchaseRateNB)
+        )
+    }
+}
