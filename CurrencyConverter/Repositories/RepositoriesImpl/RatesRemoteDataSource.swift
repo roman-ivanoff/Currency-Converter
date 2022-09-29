685571de665b8430fff40baa90042c1b383b32b0
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-29 10:54:22 +0300

    added RatesRemoteDataSource

---
+++ CurrencyConverter/Repositories/RepositoriesImpl/RatesRemoteDataSource.swift
@@ -0,0 +1,23 @@
+//
+//  RatesRemoteDataSource.swift
+//  CurrencyConverter
+//
+//  Created by Roman Ivanov on 29.09.2022.
+//
+
+import Foundation
+
+class RatesRemoteDataSource: RatesRemoteDataSourceProtocol {
+    let rateService = ArchiveOfExchangeRateService()
+    func fetchRates(completion: @escaping (Result<[CurrencyRate], Error>) -> Void) {
+        rateService.getExchangeRate { result in
+            switch result {
+            case .success(let rates):
+                let response = rates.exchangeRate
+                completion(.success(response.map(CurrencyRate.init(response:))))
+            case .failure(let error):
+                completion(.failure(NSError(domain: error.localizedDescription, code: -1)))
+            }
+        }
+    }
+}
