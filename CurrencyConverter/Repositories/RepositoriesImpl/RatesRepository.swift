72f6ffe10b4f13f19a6bf2b571fe76ba2107b39f
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-29 10:56:00 +0300

    added RatesRepository

---
+++ CurrencyConverter/Repositories/RepositoriesImpl/RatesRepository.swift
@@ -0,0 +1,36 @@
+//
+//  RatesRepository.swift
+//  CurrencyConverter
+//
+//  Created by Roman Ivanov on 29.09.2022.
+//
+
+import Foundation
+
+class RatesRepository: RatesRepositoryProtocol {
+    private let localDataSource: RatesLocalDataSource
+    private let remoteDataSource: RatesRemoteDataSource
+
+    init(localDataSource: RatesLocalDataSource, remoteDataSource: RatesRemoteDataSource) {
+        self.localDataSource = localDataSource
+        self.remoteDataSource = remoteDataSource
+    }
+
+    func fetchRates(completion: @escaping (Result<Timestamped<[CurrencyRate]>, Error>) -> Void) {
+        if let localeRates = localDataSource.rates, !localeRates.createdAt.hasHourPassed {
+            print("locale")
+            completion(.success(localeRates))
+        } else {
+            remoteDataSource.fetchRates { [localDataSource] result in
+                print("remote")
+                switch result.map({ Timestamped(wrappedValue: $0) }) {
+                case let .success(rates):
+                    localDataSource.rates = rates
+                    completion(.success(rates))
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
