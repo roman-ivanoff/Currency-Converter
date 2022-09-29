942bb26f5197e064774c615b3642f0e98b5b4b14
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-29 10:22:30 +0300

    added UserDefaultsKeys

---
+++ CurrencyConverter/Resources/UserDefaultsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsKeys.swift
+//  CurrencyConverter
+//
+//  Created by Roman Ivanov on 29.09.2022.
+//
+
+import Foundation
+
+class UserDefaultsKeys {
+    static let lastUpdateDate = "lastUpdateDate"
+    static let currencyRate = "currencyRate"
+}
