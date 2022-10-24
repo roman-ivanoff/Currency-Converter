51cf4615f2aafa6e507c340440d61917789f49da
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-10-24 17:28:46 +0300

    added tableViewCell

---
+++ CurrencyConverter/Views/CurrencyTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyTableViewCell.swift
+//  CurrencyConverter
+//
+//  Created by Roman Ivanov on 24.10.2022.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+    @IBOutlet weak var currencyTextField: UITextField!
+    @IBOutlet weak var currencyNameLabel: UILabel!
+
+}
