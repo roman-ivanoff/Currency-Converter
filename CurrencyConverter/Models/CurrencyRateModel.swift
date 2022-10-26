4a477a5e5f7b14b66f3e473addeb4df1d5222286
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-10-26 17:09:43 +0300

    added getAllRatesSections method

--- CurrencyConverter/Models/CurrencyRateModel.swift
+++ CurrencyConverter/Models/CurrencyRateModel.swift
@@ -10,6 +10,7 @@ import Foundation
 class CurrencyRateModel {
     var currencyRate: [CurrencyRate]?
     var popularCurrencies: [CurrencyRate] = []
+    var allCurrenciesInSections: [Section] = []
     var lastUpdateDate: Date?
     let repository = RatesRepository(
         localDataSource: RatesLocalDataSource(),
@@ -26,6 +27,10 @@ class CurrencyRateModel {
             case let .success(rates):
                 self.popularCurrencies = self.getPopularCurrencies(currencies: rates.wrappedValue)
                 self.lastUpdateDate = rates.createdAt
+                self.allCurrenciesInSections = self.getAllRatesSections(
+                    popularCurrencies: self.popularCurrencies,
+                    currencies: rates.wrappedValue
+                )
                 completion(.success(rates))
             case let .failure(error):
                 completion(.failure(error))
@@ -36,6 +41,21 @@ class CurrencyRateModel {
     func getPopularCurrencies(currencies: [CurrencyRate]) -> [CurrencyRate] {
         let popularRates = ["UAH", "USD", "EUR"]
         return currencies.filter { popularRates.contains($0.currency.rawValue) }
+    }
+
+    func getAllRatesSections(popularCurrencies: [CurrencyRate], currencies: [CurrencyRate]) -> [Section] {
+        var sections = [Section]()
+
+        let groupedDictionary = Dictionary(grouping: currencies, by: { $0.currency.rawValue.prefix(1) })
+        let sortedRates = groupedDictionary.sorted { $0.0 < $1.0 }
+
+        sections.append(Section(sectionName: "Popular", sectionObjects: popularCurrencies))
+
+        for (key, value) in sortedRates {
+            sections.append(Section(sectionName: String(key), sectionObjects: value))
+        }
+
+        return sections
     }
 
     private func getCurrecyByName(name: String, currencies: [CurrencyRate]) -> CurrencyRate? {
