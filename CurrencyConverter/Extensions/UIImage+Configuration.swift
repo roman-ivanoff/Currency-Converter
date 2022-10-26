66ceec26bf6b012ed919f8cb6ce7a82f5c88a889
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-10-26 14:14:45 +0300

    added UIImage extension

---
+++ CurrencyConverter/Extensions/UIImage+Configuration.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+Configuration.swift
+//  CurrencyConverter
+//
+//  Created by Roman Ivanov on 26.10.2022.
+//
+
+import UIKit
+
+extension UIImage {
+    static func imageWithColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
+    }
+}
--- CurrencyConverter/Extensions/UISegmentedControl+Configuration.swift
+++ CurrencyConverter/Extensions/UISegmentedControl+Configuration.swift
@@ -9,24 +9,13 @@ import UIKit
 
 extension UISegmentedControl {
     func removeBorders() {
-        setBackgroundImage(imageWithColor(color: backgroundColor ?? .clear), for: .normal, barMetrics: .default)
-        setBackgroundImage(imageWithColor(color: tintColor!), for: .selected, barMetrics: .default)
+        setBackgroundImage(UIImage.imageWithColor(color: backgroundColor ?? .clear), for: .normal, barMetrics: .default)
+        setBackgroundImage(UIImage.imageWithColor(color: tintColor!), for: .selected, barMetrics: .default)
         setDividerImage(
-            imageWithColor(color: UIColor.clear),
+            UIImage.imageWithColor(color: UIColor.clear),
             forLeftSegmentState: .normal,
             rightSegmentState: .normal,
             barMetrics: .default
         )
-    }
-
-    private func imageWithColor(color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()
-        context!.setFillColor(color.cgColor)
-        context!.fill(rect)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return image!
     }
 }
