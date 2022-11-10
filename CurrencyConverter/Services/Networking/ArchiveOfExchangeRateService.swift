fdadd43615f2cdb0a69e4c3c1ab3e19e6763a956
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-11-10 15:32:54 +0200

    added debug

--- CurrencyConverter/Services/Networking/ArchiveOfExchangeRateService.swift
+++ CurrencyConverter/Services/Networking/ArchiveOfExchangeRateService.swift
@@ -20,6 +20,66 @@ class ArchiveOfExchangeRateService {
         date: Date,
         completion: @escaping(Result<ArchiveOfExchangeRate, QueryServiceError>) -> Void
     ) {
+        #if DEBUG
+
+        if ProcessInfo.processInfo.arguments.contains("mock") {
+            let sampleData = ArchiveOfExchangeRate(
+                date: "09.11.2022",
+                bank: "PB",
+                baseCurrency: 980,
+                baseCurrencyLit: .uah,
+                exchangeRate: [
+                    ExchangeRate(
+                        baseCurrency: .uah,
+                        saleRateNB: 23.6763000,
+                        purchaseRateNB: 23.6763000,
+                        currency: nil,
+                        saleRate: nil,
+                        purchaseRate: nil
+                    ),
+                    ExchangeRate(
+                        baseCurrency: .uah,
+                        saleRateNB: 36.5375000,
+                        purchaseRateNB: 36.5375000,
+                        currency: "EUR",
+                        saleRate: 39.7000000,
+                        purchaseRate: 38.7000000
+                    ),
+                    ExchangeRate(
+                        baseCurrency: .uah,
+                        saleRateNB: 36.5686000,
+                        purchaseRateNB: 36.5686000,
+                        currency: "USD",
+                        saleRate: 40.2000000,
+                        purchaseRate: 39.7000000
+                    ),
+                    ExchangeRate(
+                        baseCurrency: .uah,
+                        saleRateNB: 1.0000000,
+                        purchaseRateNB: 1.0000000,
+                        currency: "UAH",
+                        saleRate: nil,
+                        purchaseRate: nil
+                    ),
+                    ExchangeRate(
+                        baseCurrency: .uah,
+                        saleRateNB: 7.7847000,
+                        purchaseRateNB: 7.7847000,
+                        currency: "PLN",
+                        saleRate: 8.5700000,
+                        purchaseRate: 7.8000000
+                    )
+                ]
+            )
+
+            DispatchQueue.main.async {
+                completion(.success(sampleData))
+            }
+
+            return
+        }
+
+        #endif
         dataTask?.cancel()
 
         let exchangeRateDate = getStringFromDate(date: date)
